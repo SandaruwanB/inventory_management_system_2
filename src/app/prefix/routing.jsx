d6768c400290a460a7f1302c5ca66786db94bc46
SandaruwanB <sandarusbandara110@gmail.com>
2024-05-27 00:36:52 +0530

    [DEV] home page route

--- src/app/prefix/routing.jsx
+++ src/app/prefix/routing.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from '../pages/login'
+import Home from '../pages/home'
 
 function Routing() {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path='/login' element={<Login />} />
         </Routes>
     </BrowserRouter>
   )
