5edec73230573ecb92442224d49e7a56711910db
SandaruwanB <sandarusbandara110@gmail.com>
2024-05-27 01:20:59 +0530

    [DEV] set loader timeout

--- src/app/pages/home.jsx
+++ src/app/pages/home.jsx
@@ -1,12 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Loader from '../components/loader';
 
 function Home() {
     const [state, setState] = useState(true);
 
+    useEffect(()=>{
+        const timeout = setTimeout(()=>{
+            setState(false);
+        },2000);
+    })
+
   return (
     <>
-        <Loader />
+        {
+            state ?
+                <Loader/>
+            :
+                <div>Home Page</div>
+        }
     </>
     
   )
