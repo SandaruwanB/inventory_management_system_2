6514da6d694be4c7d931e824c4a65daf93390884
SandaruwanB <sandarusbandara110@gmail.com>
2024-05-28 01:11:11 +0530

    [DEV] nav bar styling

--- src/app/layouts/homeHeader.jsx
+++ src/app/layouts/homeHeader.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Icon } from '@iconify/react'
 
 const HomeHeader = () => {
   return (
-    <nav className='flex fixed flex-1 justify-between md:px-10 md:py-5 p-3 bg-transparent top-0 left-0 w-screen'>
-        <div>hello</div>
-        <div>burger</div>
+    <nav className='bg-transparent fixed w-full z-20 top-0 start-0'>
+        <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+            <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <span class="self-center text-2xl font-semibold whitespace-nowrap">InventoryManagement</span>
+            </a>
+            <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+                    <Icon icon="solar:hamburger-menu-bold" className='' />
+                </button>                
+            </div>
+        </div>
     </nav>
   )
 }
