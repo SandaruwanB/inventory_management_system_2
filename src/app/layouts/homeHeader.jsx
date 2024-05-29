9ecbd228d7220853642956be59c5bf1832cde691
SandaruwanB <sandarusbandara110@gmail.com>
2024-05-29 18:16:05 +0530

    [DEV] redesign

--- src/app/layouts/homeHeader.jsx
+++ src/app/layouts/homeHeader.jsx
@@ -7,15 +7,13 @@ const HomeHeader = () => {
     }
 
   return (
-    <nav className='fixed w-full z-20 top-0 start-0'>
-        <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-            <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                <span className="self-center text-2xl font-semibold text-blue-950 whitespace-nowrap hover:tracking-wider transition-all delay-150">Stockify</span>
+    <nav className='fixed backdrop-blur-xl w-full z-20 top-0 start-0 bg-transparent opacity-70'>
+        <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-8 px-5'>
+            <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse hover-animated-text hover:text-sky-200">
+                <span className="self-center text-2xl uppercase hover:text-sky-200 font-semibold text-white whitespace-nowrap tracking-wider transition-all delay-150">Stockify</span>
             </a>
             <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <button className='bg-transparent text-blue-950 md:text-white hover:ring-2 rounded-lg' onClick={()=>burgerClicked()}>
-                    <Icon icon="solar:hamburger-menu-bold" fontSize={35}/>
-                </button>                
+                <a href="/login" className='bg-transparent px-4 py-2 text-white border-2 hover:font-semibold border-sky-200 hover:ring-2 rounded-lg'>Sign In</a>
             </div>
         </div>
     </nav>
--- src/app/pages/home.jsx
+++ src/app/pages/home.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import Loader from '../components/loader';
 import HomeHeader from '../layouts/homeHeader';
+import { Icon } from '@iconify/react';
 
 function Home() {
     const [loadingState, setLoadingState] = useState(true);
+    const [isPasswordField, setIsPasswordField] = useState(true);
 
     useEffect(()=>{
         document.title = "Stockify | Home";
         setTimeout(()=>{
             setLoadingState(false);
         },2000);
-    })
+    }, [loadingState])
 
   return (
     <>
@@ -20,21 +22,9 @@ function Home() {
             :
                 <>
                     <HomeHeader />
-                    <div className='flex md:flex-row flex-col h-screen'>
-                        <div className='md:basis-7/12 h-full'>
-                            <div className='h-full flex text-center justify-center items-center'>
-                                <span>hello world</span>
-                            </div>
-                        </div>
-                        <div className='md:basis-5/12 h-full bg-cyan-700'>
-                            <div className='h-full flex md:justify-start justify-center items-center p-4'>
-                                <div className='w-auto md:w-4/5 flex flex-col'>
-                                    <h2 className='text-center text-white text-3xl font-bold'>Sign In</h2>
-                                    <input type="text" placeholder='Email' className='mt-12 w-full' />
-                                </div>                                
-                            </div>
-                        </div>
-                    </div>
+                    <section className='p-4 min-h-screen bg-blue-950'>  
+                        <h1>daskhjdhaj</h1>                
+                    </section>
                 </>
         }
     </>
