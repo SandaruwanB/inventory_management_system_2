b064d6025490d8731f5eda5a29379f975638ad8c
SandaruwanB <sandarusbandara110@gmail.com>
2024-06-05 13:24:33 +0530

    [DEV] restyle header

--- src/app/pages/home.jsx
+++ src/app/pages/home.jsx
@@ -21,15 +21,15 @@ function Home() {
                 <Loader color={"rgb(23,37,84)"}/>
             :
                 <>
+                    <HomeHeader />
                     <main className='w-full h-screen bg-blue-950'>
-                        <div className='max-w-screen-xl mx-auto p-3'>
-                            <HomeHeader />
-                            <div className='h-full w-full flex-col xl:flex-col'>
-                                <div className='basis-1 xl:basis-7/12 xl:order-1 order-2 h-full'>
-                                    sdfads
-                                </div>
-                                <div className='basis-1 xl:basis-5/12 xl:order-2 order-1'>
-                                    skjldkj
+                        <div className='max-w-screen-xl h-full mx-auto p-3'>
+                            <div className='h-full'>
+                                <div className='flex flex-row justify-center items-center'>
+                                    <div className='order-2 basis-5/12 bg-red-600'>
+                                        sdhjshds
+                                    </div>
+                                    <div className='order-1 basis-7/12'>dsjkhdjk</div>
                                 </div>
                             </div>
                         </div>
