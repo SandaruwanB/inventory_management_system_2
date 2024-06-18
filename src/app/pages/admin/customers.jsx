e26ca296af5bb74e078d98075f07a097f617743a
SandaruwanB <sandarusbandara110@gmail.com>
2024-06-19 00:05:39 +0530

    [DEV] customers page

--- src/app/pages/admin/customers.jsx
+++ src/app/pages/admin/customers.jsx
@@ -31,62 +31,66 @@ const Customers = () => {
         console.log("Delete clicked" + id);
     }
   return (
-    <div className='flex w-screen'>
-    <DashboadrdSideBar />
-    <div className='p-7 w-full'>
-        <h1 className=' mb-4 text-3xl text-gray-800 font-semibold ml-10'>Customers</h1>
-        <div className='w-full mt-10'>
-            <div className='w-full flex justify-between'>
-                <div>
-                    <button className=' bg-green-800 hover:bg-green-950 text-white font-semibold px-3 py-1 rounded'>Add New</button>
-                </div>
-                <div className='flex'>
-                    <div className=' text-gray-800 '>
-                        showing results <span className='text-blue-950 font-bold'>{customers.length}</span>
-                    </div>                       
-                </div>
-            </div>
-            <table className='w-full table-fixed border-collapse mt-3'>
-                <thead className='bg-gray-200 border-b-2 border-gray-400'>
-                    <tr>
-                        <th className='p-3 text-sm font-semibold tracking-wide text-left w-10'>No.</th>
-                        <th className='p-3 text-sm font-semibold tracking-wide text-left'>Name</th>
-                        <th className='p-3 text-sm font-semibold tracking-wide text-left'>Email Address</th>
-                        <th className='p-3 text-sm font-semibold tracking-wide text-left'>Contact</th>
-                        <th className='p-3 text-sm font-semibold tracking-wide text-left'>Location</th>
-                        <th className='p-3 text-sm font-semibold tracking-wide text-left'>Actions</th>
-                    </tr>
-                </thead>
-                <tbody className=' divide-y divide-gray-200'>
-                    {
-                        customers.length > 0 ?
-                        customers.map((value, index)=>{
-                            return (
-                                <tr className={(index % 2) === 0 ? 'bg-white' : 'bg-gray-100'} key={index}>
-                                    <td className='p-3 text-sm text-gray-700'>{index + 1}</td>
-                                    <td className='p-3 text-sm text-gray-700'>{value.name}</td>
-                                    <td className='p-3 text-sm text-gray-700'>{value.email}</td>
-                                    <td className='p-3 text-sm text-gray-700'>{value.contact}</td>
-                                    <td className='p-3 text-sm text-gray-700'>{value.address}</td>
-                                    <td className='p-3 text-sm text-gray-700'>
-                                        <button className='hover:text-green-500' onClick={()=>editCustomer(index)}><Icon icon="basil:edit-solid" width={26} /></button>
-                                        <button className='ml-4 hover:text-red-500' onClick={()=>removeCustomer(index)}><Icon icon="material-symbols-light:delete"  width={28}/></button>
+    <>
+        <DashboadrdSideBar />
+        <div className="p-4 sm:ml-64">
+            <div className="p-4">
+                <div className='w-full'>
+                    <h1 className=' mb-4 text-3xl text-gray-800 font-semibold ml-10'>Customers</h1>
+                    <div className='w-full mt-10'>
+                        <div className='w-full flex justify-between'>
+                            <div>
+                                <button className=' bg-green-800 hover:bg-green-950 text-white font-semibold px-3 py-1 rounded'>Add New</button>
+                            </div>
+                            <div className='flex'>
+                                <div className=' text-gray-800 '>
+                                    showing results <span className='text-blue-950 font-bold'>{customers.length}</span>
+                                </div>                       
+                            </div>
+                        </div>
+                        <table className='w-full table-fixed border-collapse mt-3'>
+                            <thead className='bg-gray-200 border-b-2 border-gray-400'>
+                                <tr>
+                                    <th className='p-3 text-sm font-semibold tracking-wide text-left w-10'>No.</th>
+                                    <th className='p-3 text-sm font-semibold tracking-wide text-left'>Name</th>
+                                    <th className='p-3 text-sm font-semibold tracking-wide text-left'>Email Address</th>
+                                    <th className='p-3 text-sm font-semibold tracking-wide text-left'>Contact</th>
+                                    <th className='p-3 text-sm font-semibold tracking-wide text-left'>Location</th>
+                                    <th className='p-3 text-sm font-semibold tracking-wide text-left'>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody className=' divide-y divide-gray-200'>
+                                {
+                                    customers.length > 0 ?
+                                    customers.map((value, index)=>{
+                                        return (
+                                            <tr className={(index % 2) === 0 ? 'bg-white' : 'bg-gray-100'} key={index}>
+                                                <td className='p-3 text-sm text-gray-700'>{index + 1}</td>
+                                                <td className='p-3 text-sm text-gray-700'>{value.name}</td>
+                                                <td className='p-3 text-sm text-gray-700'>{value.email}</td>
+                                                <td className='p-3 text-sm text-gray-700'>{value.contact}</td>
+                                                <td className='p-3 text-sm text-gray-700'>{value.address}</td>
+                                                <td className='p-3 text-sm text-gray-700'>
+                                                    <button className='hover:text-green-500' onClick={()=>editCustomer(index)}><Icon icon="basil:edit-solid" width={26} /></button>
+                                                    <button className='ml-4 hover:text-red-500' onClick={()=>removeCustomer(index)}><Icon icon="material-symbols-light:delete"  width={28}/></button>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                :
+                                <tr className='bg-white'>
+                                    <td className='text-center text-blue-400 hover:underline cursor-pointer text-sm p-3' colSpan={6}>
+                                        <p>No customers found.</p>
                                     </td>
                                 </tr>
-                            )
-                        })
-                    :
-                    <tr className='bg-white'>
-                        <td className='text-center text-blue-400 hover:underline cursor-pointer text-sm p-3' colSpan={6}>
-                            <p>No customers found.</p>
-                        </td>
-                    </tr>
-                    }
-                </tbody>
-            </table>
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
+    </>
   )
 }
 
