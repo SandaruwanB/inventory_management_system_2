dc9c9b6ec4db5986e0fd07cc22186f854cf5b2ad
SandaruwanB <sandarusbandara110@gmail.com>
2024-06-28 20:09:57 +0530

    [DEV] suplier functions

--- src/app/pages/admin/supliers.jsx
+++ src/app/pages/admin/supliers.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import DashboadrdSideBar from '../../layouts/dashboadrdSideBar';
+import { confirmDialog, ConfirmDialog } from 'primereact/confirmdialog';
+import { ToastContainer, toast } from 'react-toastify';
 import { Icon } from '@iconify/react';
 import axios from 'axios';
 import { apiConfig } from '../../../apiConfig';
@@ -7,19 +9,62 @@ import { apiConfig } from '../../../apiConfig';
 const Supliers = () => {
     document.title = "Stockify | Supliers";
     const [supliers, setSupliers] = useState([]);
+    const [popupvisibility, setPopupvisibility] = useState(false);
 
     useEffect(()=>{
         axios.get(`${apiConfig.url}/api/supliers/all`).then(result=>{
             setSupliers(result.data);
         });
-    }, [])
+    }, []);
 
     const editSuplier = (id)=>{
         console.log("Edit clicked" + id);
     }
 
     const removeSupllier = (id)=>{
-        console.log("Delete clicked" + id);
+        setPopupvisibility(true);
+        confirmDialog({
+            message : 'Are you sure you want to remove this suplier?',
+            header : 'Confirmation',
+            icon : 'pi pi-exclamation-triangle',
+            accept : ()=>deleteSuplier(id),
+            reject : ()=>{},
+            rejectClassName : 'mr-2 bg-transparent',
+            acceptClassName : 'bg-red-600 text-white px-3 py-1 hover:bg-red-700'
+        });
+    }
+
+    const deleteSuplier = async (id)=>{
+        setPopupvisibility(false);
+
+        await axios.delete(`${apiConfig.url}/api/supliers/delete/${id}`).then((result)=>{
+            if (result.status === 200){
+                toast.info('Successfully Removed!', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+                const remainingSupliers = supliers.filter((result)=>result.id !== id);
+                setSupliers(remainingSupliers); 
+            }
+        }).catch(err=>{
+            toast.info('Cannot remove, This suplier related to other transactions.!', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+        });
+
     }
 
     return (
@@ -65,8 +110,9 @@ const Supliers = () => {
                                                 <td className='p-3 text-sm text-gray-700'>{value.companyname}</td>
                                                 <td className='p-3 text-sm text-gray-700'>{value.city}</td>
                                                 <td className='p-3 text-sm text-gray-700'>
-                                                    <button className='hover:text-green-500' onClick={()=>editSuplier(index)}><Icon icon="basil:edit-solid" width={26} /></button>
-                                                    <button className='ml-4 hover:text-red-500' onClick={()=>removeSupllier(index)}><Icon icon="material-symbols-light:delete"  width={28}/></button>
+                                                    <button className='hover:text-green-500' onClick={()=>editSuplier(value.id)}><Icon icon="basil:edit-solid" width={26} /></button>
+                                                    <button className='ml-4 hover:text-red-500' onClick={()=>removeSupllier(value.id)}><Icon icon="material-symbols-light:delete"  width={28}/></button>
+                                                    <ConfirmDialog visible={popupvisibility} />
                                                 </td>
                                             </tr>
                                         )
@@ -84,6 +130,7 @@ const Supliers = () => {
                 </div>
             </div>
         </div>
+        <ToastContainer />
     </>
   )
 }
