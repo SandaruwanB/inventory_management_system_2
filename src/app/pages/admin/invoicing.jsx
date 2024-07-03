005d69a86c7453d25b71d7854945093370e26868
SandaruwanB <sandarusbandara110@gmail.com>
2024-07-03 23:44:28 +0530

    [DEV] invoicing tree

--- src/app/pages/admin/invoicing.jsx
+++ src/app/pages/admin/invoicing.jsx
@@ -1,46 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Icon } from '@iconify/react';
 import DashboadrdSideBar from '../../layouts/dashboadrdSideBar';
+import { confirmDialog, ConfirmDialog } from 'primereact/confirmdialog';
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
+import { apiConfig } from '../../../apiConfig';
 
 const Invoicing = () => {
     document.title = "Stockify | Invoicing";
 
-    const invoices = [
-        {
-            "name" : "INV123456",
-            "customer" : "Nimal Shantha",
-            "amount" : "12700.00",
-            "contact" : "07611562233",
-            "status" : "draft",
-            "date" : "2024-06-23",
-            "image" : "/assets/images/defaultUser.png"
-        },
-        {
-            "name": "INV123457",
-            "customer": "Kasun Nishantha",
-            "amount": "22500.00",
-            "contact": "123-456-7890",
-            "status" : "posted",
-            "date" : "2024-06-23",
-            "image": "/assets/images/defaultUser.png"
-        },
-        {
-            "name": "INV123457",
-            "customer": "Nimal Susantha",
-            "amount": "23450.00",
-            "contact": "987-654-3210",
-            "status" : "canceled",
-            "date" : "2024-06-23",
-            "image": "/assets/images/defaultUser.png"
-        }
-    ]
+    const [invoices, setInvoices] = useState([]);
+    const [popupvisibility, setPopupvisibility] = useState(false);
 
-    const editEmployee = (id)=>{
+    useEffect(()=>{
+        axios.get(`${apiConfig.url}/api/invoicing/all`).then(result=>{
+            setInvoices(result.data);
+            console.log(result.data);
+        })
+    },[]);
+
+    const editInvoice = (id)=>{
         console.log("Edit clicked" + id);
     }
 
-    const removeEmployee = (id)=>{
-        console.log("Delete clicked" + id);
+    const removeInvoice = (id)=>{
+        setPopupvisibility(true);
+        confirmDialog({
+            message : 'Are you sure you want to remove this invoice?',
+            header : 'Confirmation',
+            icon : 'pi pi-exclamation-triangle',
+            accept : ()=>deleteInvoice(id),
+            reject : ()=>{setPopupvisibility(false)},
+            rejectClassName : 'mr-2 bg-transparent',
+            acceptClassName : 'bg-red-600 text-white px-3 py-1 hover:bg-red-700'
+        });
+    }
+
+    const deleteInvoice = async (id)=>{
+        setPopupvisibility(false);
+        console.log(id);
+        await axios.delete(`${apiConfig.url}/api/invoicing/delete/${id}`).then(()=>{
+            toast.info('Successfully Removed!', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            const remainingInvoices = invoices.filter((result)=>result.id !== id);
+            setInvoices(remainingInvoices);
+        });
     }
 
     return (
@@ -80,8 +92,8 @@ const Invoicing = () => {
                                         return (
                                             <tr className={(index % 2) === 0 ? 'bg-white' : 'bg-gray-100'} key={index}>
                                                 <td className='p-3 text-sm text-gray-700'>{index + 1}</td>
-                                                <td className='p-3 text-sm text-gray-700'>{value.name}</td>
-                                                <td className='p-3 text-sm text-gray-700'>{value.customer}</td>
+                                                <td className='p-3 text-sm text-gray-700'>{value.invoicenumber}</td>
+                                                <td className='p-3 text-sm text-gray-700'>{value.customer.firstname + " " + value.customer.lastname}</td>
                                                 <td className='p-3 text-sm text-gray-700'>Rs. {value.amount}</td>
                                                 <td className={`p-3 text-sm text-white`}> 
                                                     <span className={`${value.status === "posted" ? "bg-green-500" : value.status === "canceled" ? "bg-red-500" : "bg-yellow-500"} px-2 py-[3px] rounded-md`}>
@@ -90,15 +102,16 @@ const Invoicing = () => {
                                                 </td>
                                                 <td className='p-3 text-sm text-gray-700'>{value.date}</td>
                                                 <td className='p-3 text-sm text-gray-700'>
-                                                    <button className='hover:text-green-500' onClick={()=>editEmployee(index)}><Icon icon="basil:edit-solid" width={26} /></button>
-                                                    <button className='ml-4 hover:text-red-500' onClick={()=>removeEmployee(index)}><Icon icon="material-symbols-light:delete"  width={28}/></button>
+                                                    <button className='hover:text-green-500' onClick={()=>editInvoice(value.id)}><Icon icon="basil:edit-solid" width={26} /></button>
+                                                    <button className='ml-4 hover:text-red-500' onClick={()=>removeInvoice(value.id)}><Icon icon="material-symbols-light:delete"  width={28}/></button>
+                                                    <ConfirmDialog visible={popupvisibility} />
                                                 </td>
                                             </tr>
                                         )
                                     })
                                 :
                                 <tr className='bg-white'>
-                                    <td className='text-center text-blue-400 hover:underline cursor-pointer text-sm p-3' colSpan={6}>
+                                    <td className='text-center text-blue-400 hover:underline cursor-pointer text-sm p-3' colSpan={7}>
                                         <p>No invoices found.</p>
                                     </td>
                                 </tr>
@@ -109,6 +122,7 @@ const Invoicing = () => {
                 </div>
             </div>
         </div>
+        <ToastContainer />
     </>
   )
 }
