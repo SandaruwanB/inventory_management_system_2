6197ad78f2c8f4c7a2c6edffc178cdcd84206d07
SandaruwanB <sandarusbandara110@gmail.com>
2024-08-02 22:27:44 +0530

    [DEV] sales report view

---
+++ src/app/components/salesReport.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+
+const style = StyleSheet.create({
+    page : {
+        border : '1px solid black',
+        padding : 30,
+    },
+    topContent : {
+        width : '100%',
+        height : '100%',
+        backgroundColor : 'red',
+    },
+    header : {
+        width : '100%',
+        textAlign : 'center',
+        textTransform : 'uppercase',
+    },
+    rightContent : {
+        display : 'flex',
+        alignSelf : 'flex-end',
+    },
+    textLeft : {
+        textAlign : 'left',
+    },
+    companyDetails : {
+        fontSize : 12,
+        fontWeight : 'semibold',     
+    },
+    tableContainer : {
+        position : 'relative', 
+        marginTop : 10
+    },
+    absolute : {
+        position : 'absolute',
+    },
+    smallFont : {
+        fontSize : 10
+    },
+    midFont : {
+        fontSize : 12
+    },
+    line : {
+        width : '100%',
+        padding : 0.5,
+        backgroundColor : 'gray',
+        position : 'absolute',
+        top : 16,
+    }
+});
+
+const SalesReport = () => (
+    <Document>
+        <Page size={'A4'} style={style.page}>
+            <View style={style.header}>
+                <Text>Sales Details Report</Text>
+            </View>
+        </Page>
+    </Document>
+)
+
+export default SalesReport
--- src/app/pages/admin/salesReport.jsx
+++ src/app/pages/admin/salesReport.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DashboadrdSideBar from '../../layouts/dashboadrdSideBar';
+import axios from 'axios';
+import { apiConfig } from '../../../apiConfig';
 
 
 const SalesReport = () => {
+    const [sales, setSales] = useState([]);
+
+    useEffect(()=>{
+        axios.get(`${apiConfig.url}/api/orders/customer`).then((result)=>{
+            setSales(result.data);
+            console.log(result.data);
+        })
+    },[])
+
+
   return (
     <>
         <DashboadrdSideBar />
@@ -21,8 +33,43 @@ const SalesReport = () => {
                                 </div>
                             </div>
                         </div>
-                        <div>
-                            <h1>asdhasjd</h1>
+                        <div className='mt-10 w-full'>
+                            <div className='grid grid-cols-3 gap-2'>
+                                <div className='col-span-2 h-full'>
+                                    <table className='w-full table-fixed border-collapse mt-3'>
+                                        <thead className='bg-gray-200 border-b-2 border-gray-400'>
+                                            <tr>
+                                                <th className='p-3 text-sm font-semibold tracking-wide text-left w-10'>No.</th>
+                                                <th className='p-3 text-sm font-semibold tracking-wide text-left'>Customer</th>
+                                                <th className='p-3 text-sm font-semibold tracking-wide text-left'>Date</th>
+                                                <th className='p-3 text-sm font-semibold tracking-wide text-right'>Amount</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className=' divide-y divide-gray-200'>
+                                            {
+                                                sales.length > 0 ?
+                                                sales.map((value, index)=>{
+                                                    return (
+                                                        <tr className={(index % 2) === 0 ? 'bg-white' : 'bg-gray-100'} key={index}>
+                                                            <td className='p-3 text-sm text-gray-700'>{index + 1}</td>
+                                                            <td className='p-3 text-sm text-gray-700'>{value.customer.firstname + " " + value.customer.lastname}</td>
+                                                            <td className='p-3 text-sm text-gray-700'>{value.date}</td>
+                                                            <td className='p-3 text-sm text-gray-700 text-right'>{}</td>
+                                                        </tr>
+                                                    )
+                                                })
+                                                :
+                                                <tr className='bg-white'>
+                                                    <td className='text-center text-blue-400 hover:underline cursor-pointer text-sm p-3' colSpan={4}>No Sales Details Found</td>
+                                                </tr>
+                                            }
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div>
+                                    sdfsdf
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
