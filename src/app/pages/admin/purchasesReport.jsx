e053bd2e00099802f7ddd275f7963b9e4ffde7ac
SandaruwanB <sandarusbandara110@gmail.com>
2024-08-03 00:12:41 +0530

    [DEV] purchasing view form

---
+++ src/app/pages/admin/purchasesReport.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import DashboadrdSideBar from '../../layouts/dashboadrdSideBar';
+import axios from 'axios';
+import { apiConfig } from '../../../apiConfig';
+
+const PurchasesReport = () => {
+    document.title = "Stokify | Purchases Report";
+
+    const [purchasing, setPurchasings] = useState([]);
+
+    useEffect(()=>{
+        axios.get(`${apiConfig.url}/api/orders/suplier`).then(result=>{
+            setPurchasings(result.data);
+        })
+    },[]);
+    
+  return (
+    <>
+        <DashboadrdSideBar />
+        <div className="p-4 sm:ml-64">
+            <div className="p-4">
+                <div className='w-full'>
+                    <h1 className='mb-4 text-3xl text-gray-800 font-semibold'>Purchasing Report</h1>
+                    <div className='w-full mt-10'>
+                        <div className='w-full flex justify-between'>
+                            <div>
+                                <button className='bg-green-800 hover:bg-green-950 text-white font-semibold px-3 py-1 rounded'>Download PDF</button>
+                            </div>
+                            <div className='flex'>
+                                <div className='text-gray-800 ml-4'>
+                                   
+                                </div>
+                            </div>
+                        </div>
+                        <div className='mt-10 w-full'>
+                            <div className='grid grid-cols-2 gap-10'>
+                                <div className='h-full'>
+                                    <table className='w-full table-fixed border-collapse mt-3'>
+                                        <thead className='bg-gray-200 border-b-2 border-gray-400'>
+                                            <tr>
+                                                <th className='p-3 text-sm font-semibold tracking-wide text-left w-10'>No.</th>
+                                                <th className='p-3 text-sm font-semibold tracking-wide text-left'>Suplier</th>
+                                                <th className='p-3 text-sm font-semibold tracking-wide text-left'>Date</th>
+                                                <th className='p-3 text-sm font-semibold tracking-wide text-right'>Amount</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className=' divide-y divide-gray-200'>
+                                            {
+                                                purchasing.length > 0 ?
+                                                purchasing.map((value, index)=>{
+                                                    return (
+                                                        <tr className={(index % 2) === 0 ? 'bg-white' : 'bg-gray-100'} key={index}>
+                                                            <td className='p-3 text-sm text-gray-700'>{index + 1}</td>
+                                                            <td className='p-3 text-sm text-gray-700'>{value.customer.firstname + " " + value.customer.lastname}</td>
+                                                            <td className='p-3 text-sm text-gray-700'>{value.date}</td>
+                                                            <td className='p-3 text-sm text-gray-700 text-right'>
+                                                                {value.ordermove.map((line, lineIndex) => (
+                                                                    <div key={lineIndex}>
+                                                                        Rs.{ line.itemcount * line.product.unitprice }
+                                                                    </div>
+                                                                ))}
+                                                            </td>
+                                                        </tr>
+                                                    )
+                                                })
+                                                :
+                                                <tr className='bg-white'>
+                                                    <td className='text-center text-blue-400 hover:underline cursor-pointer text-sm p-3' colSpan={4}>No Purchasing Details Found</td>
+                                                </tr>
+                                            }
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default PurchasesReport
